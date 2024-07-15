8c9303cb1443565373b7c091346e42e03aff718e
maiwann <smogo@maiwann.net>
2024-07-15 17:13:24 +0200

    changement url bouton vers atelier Scribouilli PAS 2024

--- assets/scripts/components/screens/Welcome.svelte
+++ assets/scripts/components/screens/Welcome.svelte
@@ -11,7 +11,7 @@
        <h2>Le Nouveau Front Populaire a gagné, youpi !</h2>
       <p>Scribouilli 2024 se ferme donc, si vous voulez l'utiliser pour créer un petit site pour votre association, c'est possible sur <a href="https://atelier.scribouilli.org/">https://atelier.scribouilli.org/</a></p>
 
-      <a href="./choose-account" class="btn">C'est parti !</a>
+      <a href="https://atelier.scribouilli.org/" class="btn">Aller sur Scribouilli</a>
     {:else if isFetchingCurrentUserRepos }
       <h2>Nous regardons si vous avez plusieurs sites Scribouilli…</h2>
       <img
